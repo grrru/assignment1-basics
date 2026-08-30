8fabd2d39fbfeb95712b4a8b486ab1c9d8f4c6d3
grrru <gruns0989@naver.com>
2026-08-30 17:45:43 +0900

    Implemet BPE merge

--- cs336_basics/bpe.py
+++ cs336_basics/bpe.py
@@ -20,7 +20,7 @@ def train_bpe(
     if vocab_size < 256 + len(special_tokens):
         raise ValueError
 
-    vocab = {}
+    vocab: dict[int, bytes] = {}
     for i in range(256):
         vocab[i] = bytes([i])
 
@@ -41,17 +41,53 @@ def train_bpe(
 
     str_pre_tokens = Counter(match.group() for split in splitted for match in regex.finditer(PAT, split))
 
-    pre_tokens = {}
+    pre_tokens: dict[tuple[bytes, ...], int] = {}
     for k, v in str_pre_tokens.items():
         bk = k.encode("utf-8")
         t = tuple(bk[i : i + 1] for i in range(len(bk)))
         pre_tokens[t] = v
 
-    print(pre_tokens)
+    # 2. merge pair
+    merges: list[tuple[bytes, bytes]] = []
+
+    while len(vocab) < vocab_size:
+        # find max count pair (count first, byte order second)
+        checked_pair: dict[tuple[bytes, ...], int] = {}
+        for k, v in pre_tokens.items():
+            for i in range(len(k) - 1):
+                pair = k[i : i + 2]
+                checked_pair[pair] = checked_pair.get(pair, 0) + v
+
+        if not checked_pair:
+            break
+        max_pair = max(checked_pair.items(), key=lambda x: (x[1], x[0]))[0]
+        merges.append((max_pair[0], max_pair[1]))
+
+        merged_pre_tokens: dict[tuple[bytes, ...], int] = {}
+        for k, v in pre_tokens.items():
+            i = 0
+            n_k: list[bytes] = []
+            while i < len(k):
+                pair = k[i : i + 2]
+                if pair == max_pair:
+                    n_k.append(b"".join(pair))
+                    i += 2
+                else:
+                    n_k.append(k[i : i + 1][0])
+                    i += 1
+
+            merged_pre_tokens[tuple(n_k)] = v
+
+        pre_tokens = merged_pre_tokens
+        vocab[len(vocab)] = b"".join(max_pair)
+
+    return vocab, merges
 
 
-train_bpe(
-    "/Users/user/workspace/stanford-cs336/assignment1-basics/tests/fixtures/tinystories_sample.txt",
-    10000,
-    ["<|endoftext|>"],
+print(
+    train_bpe(
+        "/Users/user/workspace/stanford-cs336/assignment1-basics/tests/fixtures/tinystories_sample.txt",
+        10000,
+        ["<|endoftext|>"],
+    )
 )
